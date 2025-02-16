5b61bcd31e53a9986fa1cdf13ab76ab344399f1b
Harinditha <harinditharuchirawya@yahoo.com>
2025-02-16 16:21:22 +0530

    successfull login

---
+++ tests/login.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.only('Successfull Login', async ({ page }) => {
+
+    await page.goto('https://rahulshettyacademy.com/client');
+    await page.locator('.login-title').isVisible();
+    expect(page.locator('.login-title')).toBeVisible();
+
+    const email = page.locator('#userEmail');
+    const pword = page.locator('#userPassword');
+    const loginBtn = page.locator('#login');
+
+    await email.fill('player_wrighter@gmail.com');
+    await pword.fill('Player123@');
+    await loginBtn.click();
+
+    await page.waitForLoadState('domcontentloaded');
+    const a = await page.locator('.card-body b').nth(0);
+    const productName = await a.textContent();
+    console.log(productName);
+
+
+
+
+
+});
