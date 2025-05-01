68956968412809b2029f4756931557682204c5dc
Harinditha <harinditharuchirawya@yahoo.com>
2025-05-01 10:44:47 +0530

    Code refactoring

--- pageObjects/homePage.js
+++ pageObjects/homePage.js
@@ -1,21 +1,21 @@
-// class homePage {
+class homePage {
 
-//     constructor(page){
+    constructor(page){
 
-//         this.page = page;
-//         this.productCards = page.locator('.card-body b');
+        this.page = page;
+        this.productCards = page.locator('.card-body b');
         
-//     }
+    }
 
-//     async SuccessLoginVerification(){
+    async SuccessLoginVerification(){
 
-//         await page.waitForLoadState('domcontentloaded');
-//         const productName =await this.productCards.nth(0).textContent();
-//         //const productName = await a.textContent();
-//         console.log(productName);
+        await page.waitForLoadState('domcontentloaded');
+        const productName =await this.productCards.nth(0).textContent();
+        //const productName = await a.textContent();
+        console.log(productName);
         
-//     }
+    }
 
-// }
+}
 
-// module.exports = homePage;
+module.exports = homePage;
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,12 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const loginPage = require('../pageObjects/loginPage');
-//const homePage = require('../pageObjects/homePage');
+const homePage = require('../pageObjects/homePage');
 
 test('Successfull Login', async ({ page }) => {
 
     const LoginPage = new loginPage(page);
-    //const HomePage = new homePage();
+    const HomePage = new homePage(page);
 
     //const url = 'https://rahulshettyacademy.com/client/auth/login';
     const email = 'player_wrighter@gmail.com';
@@ -16,7 +16,7 @@ test('Successfull Login', async ({ page }) => {
     await LoginPage.goto();
     await LoginPage.successLoginPageNavigation();
     await LoginPage.UserLogin(email, password);
-    //await HomePage.SuccessLoginVerification();
+    await HomePage.SuccessLoginVerification();
 
 
 });
